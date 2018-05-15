41f8ab4cafcb02f4a63ba2b64bf943b336793c57
Vaishak <vaishak.vellore@gmail.com>
2018-05-14 23:56:35 -0700

    adding staircase problem

---
+++ src/Staircase.java
@@ -0,0 +1,43 @@
+
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Staircase {
+
+    /*
+     * Complete the staircase function below.
+     */
+    static void staircase(int n) {
+        /*
+         * Write your code here.
+         */
+    	String var="#";
+      /* for (int i=0;i<n;i++)
+            { 
+            System.out.printf("%"+n+"s%n",var);
+            var=var+"#";
+        }
+        */
+    	for(int i=0 ; i<n ;i++){
+            for(int j = 0; j <= n-i-1; j++){
+                System.out.print(" ");
+            }
+            for(int j = n-i ; j< n; j++){
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine().trim());
+
+        staircase(n);
+    }
+}
