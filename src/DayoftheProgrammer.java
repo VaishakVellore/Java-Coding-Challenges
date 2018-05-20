0acbe504077be97b4240ba954fb069f1cbd991eb
Vaishak <vaishak.vellore@gmail.com>
2018-05-20 00:02:50 -0700

    updated dayoftheProgrammer problem

--- src/DayoftheProgrammer.java
+++ src/DayoftheProgrammer.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.math.*;
 import java.security.*;
 import java.text.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
@@ -9,26 +11,37 @@ import java.util.regex.*;
 public class DayoftheProgrammer {
 
     // Complete the solve function below.
-    static String solve(int year) {
-    	
+    	public static String solve(int year) {
 
-    }
+    		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    		// for all cases after 1918 as I am starting from 1st day of year
+    		int daysToAdjust = 255;
 
-    private static final Scanner scanner = new Scanner(System.in);
+    		if (year == 1918) {
+    			// this is the year they turned to gregorian calendar
+    			daysToAdjust = daysToAdjust + 13;
+    		}
+
+    		else if (year < 1918) {
+    			// jullian calendar only divide by 4 for leap year, where as gregorian excludes divide by 100 case
+    			if (year % 4 == 0 && year % 100 == 0) {
+    				daysToAdjust = daysToAdjust - 1;
+    			}
+    		}
+
+    		LocalDate date = LocalDate.of(year, 1, 1);
+    		LocalDate dayOfProgrammer = date.plusDays(daysToAdjust);
+    		return dayOfProgrammer.format(formatter);
+    	}
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
+    	Scanner scanner = new Scanner(System.in);
         int year = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         String result = solve(year);
 
-        bufferedWriter.write(result);
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
-
+        System.out.println(result);
         scanner.close();
     }
 }
