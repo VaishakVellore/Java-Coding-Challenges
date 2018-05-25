8d3fe8cd6d94d32a8c9b103f0926567c0bd1f887
Vaishak <vaishak.vellore@gmail.com>
2018-05-25 14:35:41 -0700

    updated with improved time complexity through recursion

--- src/CoinChangeProblem.java
+++ src/CoinChangeProblem.java
@@ -1,11 +1,36 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class CoinChangeProblem {
 	
-	static long getWays(long n, long[] c) 
+	static long getWays(int n, long[] c)
 	{
+		return getWays(n, c, 0, new HashMap<String, Long>());
+	}
+	
+	static long getWays(int n, long[] c, int index, HashMap<String, Long> memo) 
+	{
+		if( n < 0)
+			return 0;
+		if(n == 0)
+			return 1;
+		if( index >= c.length)
+			return 0;
 		
-
+		String key = n + "-" + index;
+		if(memo.containsKey(key))
+			return memo.get(key);
+		
+		int amountWithCoin = 0;
+		long count = 0;
+		while(amountWithCoin <= n)
+		{
+			int remaining = n - amountWithCoin;
+			count += getWays(remaining,c, index +1, memo);
+			amountWithCoin += c[index];
+		}
+		memo.put(key, count);
+		return count;
     }
 
 	public static void main(String[] args)
@@ -14,7 +39,6 @@ public class CoinChangeProblem {
 		String[] nm = s.nextLine().split(" ");
 		int n = Integer.parseInt(nm[0]);
 		int m = Integer.parseInt(nm[1]);
-		
 		long[] c = new long[m];
 
         String[] cItems = s.nextLine().split(" ");
