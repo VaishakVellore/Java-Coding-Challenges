aec98325a4b6c2927169352699e484c7e40c367f
Vaishak <vaishak.vellore@gmail.com>
2018-06-20 14:59:44 -0700

    IntroToSorting

---
+++ src/Sorting/IntrotoSorting.java
@@ -0,0 +1,25 @@
+package String;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class IntrotoSorting {
+
+    public static void main(String[] args)
+    {
+    	Scanner s = new Scanner(System.in);
+    	int V = s.nextInt();
+    	int n = s.nextInt();
+    	int i = 0;
+    	while(s.nextInt()!= V)
+    		i++;
+    	System.out.println(i);
+    	
+    }
+}
+
