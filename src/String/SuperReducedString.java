ba7e73557650a6a881e87a60812988446882fc9d
Vaishak <vaishak.vellore@gmail.com>
2018-06-20 15:03:50 -0700

    adding SuperReducedString problem

---
+++ src/String/SuperReducedString.java
@@ -0,0 +1,13 @@
+package String;
+
+import java.util.Scanner;
+
+public class SuperReducedString {
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		String str = s.nextLine();
+		
+	}
+
+}
