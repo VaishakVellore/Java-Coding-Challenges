bf5d4adff16e02406ac50c65fae23730e72e35c4
Vaishak <vaishak.vellore@gmail.com>
2018-06-21 11:02:35 -0700

    CaesarCiper problem

---
+++ src/String/CaesarCipher.java
@@ -0,0 +1,16 @@
+package String;
+
+import java.util.Scanner;
+
+public class CaesarCipher {
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		in.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		
+		String str = in.nextLine();
+		int move = in.nextInt();
+		
+	}
+}
