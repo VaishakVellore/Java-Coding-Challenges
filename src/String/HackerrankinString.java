539fe571b29d06b955ab3725e837a087eea460f7
Vaishak <vaishak.vellore@gmail.com>
2018-06-21 14:13:10 -0700

    updating problem

--- src/String/HackerrankinString.java
+++ src/String/HackerrankinString.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class HackerrankinString {
 	
+	public static String hackerrankinString(String str)
+	{
+		String find = "hackerrank";
+		StringBuilder sb = new StringBuilder(str);
+		for( int j = 0; j<str.length(); j++)
+		{
+			if(sb.charAt(j) != find.charAt(j))
+				sb.deleteCharAt(j);
+		}
+		if(sb.toString() == find)
+			return "YES";
+		else
+			return "NO";
+	}
+	
 	public static void main(String[] args)
 	{
 		Scanner in = new Scanner(System.in);
@@ -11,8 +26,8 @@ public class HackerrankinString {
 		for(int i = 0; i<n; i++)
 		{
 			String str = in.nextLine();
-			String find = "hackerrank";
-			
+			String result = hackerrankinString(str);
+			System.out.println(result);
 		}
 	}
 
