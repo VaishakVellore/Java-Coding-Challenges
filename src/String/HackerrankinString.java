f3091e6ff59a0076c8dd3df654b93ea7d8c92eb3
Vaishak <vaishak.vellore@gmail.com>
2018-06-21 13:57:31 -0700

    Hackerrank is a string problem

---
+++ src/String/HackerrankinString.java
@@ -0,0 +1,19 @@
+package String;
+
+import java.util.Scanner;
+
+public class HackerrankinString {
+	
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		for(int i = 0; i<n; i++)
+		{
+			String str = in.nextLine();
+			String find = "hackerrank";
+			
+		}
+	}
+
+}
