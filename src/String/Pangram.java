f451d63db4d98ca4d87ca0723214987b84e6abd8
Vaishak <vaishak.vellore@gmail.com>
2018-06-22 12:15:08 -0700

    adding pangram example

---
+++ src/String/Pangram.java
@@ -0,0 +1,13 @@
+package String;
+
+import java.util.Scanner;
+
+public class Pangram {
+
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		String str = in.nextLine();
+		
+	}
+}
