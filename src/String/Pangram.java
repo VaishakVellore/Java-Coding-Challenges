da94abf530fdbed262b5c531740a6954cd45b555
Vaishak <vaishak.vellore@gmail.com>
2018-06-22 12:54:50 -0700

    added contains function to comapre the array

--- src/String/Pangram.java
+++ src/String/Pangram.java
@@ -8,6 +8,21 @@ public class Pangram {
 	{
 		Scanner in = new Scanner(System.in);
 		String str = in.nextLine();
-		
+		str = str.toLowerCase();
+		String compare = "abcdefghijklmnopqrstuvwxyz";
+		char letters[] = compare.toCharArray();
+		Boolean isPangram = true;
+		for(int i = 0; i<compare.length(); i++)
+		{
+			if(str.contains(String.valueOf(letters[i]))== false)
+			{
+				isPangram= false;
+				break;
+			}
+		}
+		if(isPangram)
+			System.out.println("pangram");
+		else
+			System.out.println("not pangram");
 	}
 }
