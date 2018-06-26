0bb3749bf132a1a6220db52b07c0aeb8b698df77
Vaishak <vaishak.vellore@gmail.com>
2018-06-26 10:26:08 -0700

    weighted uniform string problem adding hashmap

---
+++ src/String/WeightedUniformStrings.java
@@ -0,0 +1,19 @@
+package String;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class WeightedUniformStrings {
+
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		String str = in.nextLine();
+		Map<Integer, Integer>hash = new HashMap<>();
+		for(int i = 0; i<str.length(); i++)
+		{
+			
+		}
+	}
+}
