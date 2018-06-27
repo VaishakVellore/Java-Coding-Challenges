40dda27e61314dd4c02ec26d70e0fd7fa48be7f7
Vaishak <vaishak.vellore@gmail.com>
2018-06-26 21:21:50 -0700

    adding updated weighted uniform string

--- src/String/WeightedUniformStrings.java
+++ src/String/WeightedUniformStrings.java
@@ -7,13 +7,28 @@ import java.util.Scanner;
 public class WeightedUniformStrings {
 
 	public static void main(String[] args)
-	{
+    {
 		Scanner in = new Scanner(System.in);
 		String str = in.nextLine();
 		Map<Integer, Integer>hash = new HashMap<>();
 		for(int i = 0; i<str.length(); i++)
 		{
-			
+			char temp = str.charAt(i);
+			int counter = 1;
+            hash.put((temp-96) * counter, 0);
+            while(i+1 < str.length() && temp == str.charAt(i+1)){
+                i++;
+                counter++;
+                hash.put((temp-96) * counter,0);
+            }
 		}
+        int n = in.nextInt();
+        for(int a0 = 0; a0 < n; a0++) {
+            int x = in.nextInt();
+            if (hash.containsKey(x))
+                System.out.println("Yes");
+            else
+                System.out.println("No");
+        }
 	}
 }
