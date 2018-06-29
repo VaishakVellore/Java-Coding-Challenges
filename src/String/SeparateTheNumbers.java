197feaf01cbbb16e5dbbbc8c46e06f0fd922fae2
Vaishak <vaishak.vellore@gmail.com>
2018-06-29 14:20:45 -0700

    updated Separate the numbers problem with recursion

--- src/String/SeparateTheNumbers.java
+++ src/String/SeparateTheNumbers.java
@@ -4,19 +4,40 @@ import java.util.Scanner;
 
 public class SeparateTheNumbers {
 
-    private static void separateNumbers(String str) {
-        
+    public static boolean find(String str, String first)
+    {
+        if(str == null)
+            return true;
+        if(str.length()==0)
+            return true;
+        String next = Long.toString(Long.parseLong(first)+1);
+        if(str.startsWith(next))
+            return find(str.substring(next.length()), next);
+        return false;
     }
+
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        for( int i = 0; i < n; i++)
+        //in.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for( int j = 0; j < n; j++)
         {
-            String str = in.nextLine();
-            separateNumbers(str);
-        }
-        in.close();
-    }
+            String str = in.next();
+            boolean flag = false;
 
+            for( int i =1; i <= str.length()/2; i++)
+            {
+                String first = str.substring(0,i);
+                if(find(str.substring(i), first))
+                {
+                    flag = true;
+                    System.out.println("YES " + first);
+                    break;
+                }
+            }
+            if(!flag)
+                System.out.println("NO");
+        }
+    }
 }
