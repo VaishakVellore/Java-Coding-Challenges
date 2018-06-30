a4e333e069f3f0c6bc28d54ddccd7ff5da506ea7
Vaishak <vaishak.vellore@gmail.com>
2018-06-29 20:13:09 -0700

    adding gemstones problem

---
+++ src/String/Gemstones.java
@@ -0,0 +1,15 @@
+package String;
+
+import java.util.Scanner;
+
+public class Gemstones {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int a = 0; a<n; a++)
+        {
+            String str = in.next();
+        }
+    }
+}
