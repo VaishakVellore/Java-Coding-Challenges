4cc5a2da899a20057065b5c1f201333e6b03d77e
Vaishak <vaishak.vellore@gmail.com>
2018-06-30 23:16:50 -0700

    adding Anagram problem

--- src/String/ALoveLetterMystery.java
+++ src/String/ALoveLetterMystery.java
@@ -10,16 +10,15 @@ public class ALoveLetterMystery {
         for (int i = 0; i < n; i++)
         {
             String str = in.next();
-            char[]c = str.toCharArray();
-            int index = 0, count = 0;
-            for( int j = str.length()-1; j>= (str.length()+1)/2; j--)
+            int count = 0, left = 0, right = str.length()-1;
+            while(left < right)
             {
-                if(c[j] != c[index])
-                {
-                    c[j]--;
-                    count++;
-                }
-                index++;
+                char l = str.charAt(left);
+                char r = str.charAt(right);
+                if((int)l != (int)r )
+                    count = count + Math.abs((int)l - (int)r);
+                left++;
+                right--;
             }
             System.out.println(count);
         }
---
+++ src/String/Anagram.java
@@ -0,0 +1,33 @@
+package String;
+
+import java.util.Scanner;
+
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            String str = in.next();
+            int len = str.length();
+            int count = 0;
+            if(len%2 !=0)
+            {
+                System.out.println(-1);
+                continue;
+            }
+            
+            String s1 = str.substring(0,len/2);
+            String s2 = str.substring(len/2, len);
+            char[] array = s1.toCharArray();
+            for(char c: array)
+            {
+                int index = s2.indexOf(c);
+                if(index == -1)
+                    count++;
+                else
+                    s2 = s2.substring(0,index)+s2.substring(index+1);
+            }
+            System.out.println(count);
+        }
+    }
+}
