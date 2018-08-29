0a7ad237d5025a1d6215a874364c74c8ee0d462f
VaishakVellore <vaishak.vellore@gmail.com>
2018-08-29 05:34:47 -0700

    Adding ArrayLeft Rotation problem

---
+++ src/InterviewPreparationkit/ArrayLeftRotation.java
@@ -0,0 +1,55 @@
+package InterviewPreparationkit;
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class ArrayLeftRotation {
+
+    // Complete the rotLeft function below.
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+        int temp = 0, j;
+        for(int i = 0; i < d; i++)
+        {
+            temp = a[0];
+            for( j = -0; j < a.length - 1; j++)
+            {
+                a[j] = a[j+1];
+            }
+            a[j] = temp;
+        }
+        for( int i = 0; i < a.length; i++)
+        {
+            System.out.print(a[i]);
+        }
+
+
+        scanner.close();
+    }
+}
+
+
