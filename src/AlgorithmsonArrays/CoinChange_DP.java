42ad55081c843220f5b112153b2e042c71444887
VaishakVellore <vaishak.vellore@gmail.com>
2019-02-09 10:41:39 -0700

    Updating CoinChange dp PROBLEM

--- src/AlgorithmsonArrays/CoinChange_DP.java
+++ src/AlgorithmsonArrays/CoinChange_DP.java
@@ -1,5 +1,24 @@
 package AlgorithmsonArrays;
 
+import java.util.Arrays;
+
 public class CoinChange_DP {
-    
+    static int countChangeDP(int[] arr, int m, int n)
+    {
+        int table[] = new int[n+1];
+        table[0] = 1;
+
+        for(int i = 0; i<m; i++)
+            for(int j = arr[i]; j <=n; j++)
+                table[j] = table[j] + table[j-arr[i]];
+
+        return table[n];
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3};
+        int m = arr.length;
+        System.out.println( countChangeDP(arr, m, 4));
+    }
 }
