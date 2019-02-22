7d4295dd27f88519c9b367452524da6702abf224
VaishakVellore <vaishak.vellore@gmail.com>
2019-02-22 12:12:31 -0700

    Adding String Matching Fidessa Problem

---
+++ src/InterviewPreparationkit/Fidessa_StringMatching.java
@@ -0,0 +1,46 @@
+package InterviewPreparationkit;
+
+import java.util.Stack;
+
+public class Fidessa_StringMatching {
+
+    public static Boolean stringMatching(String str)
+    {
+        if(str.length() == 0)
+            return false;
+        if(str.length() %2 != 0)
+            return false;
+        if(!Character.isUpperCase(str.charAt(0)))
+            return false;
+
+        Stack<Character> stack = new Stack<>();
+        //int index = -1;
+        for(int i = 0; i< str.length(); i++)
+        {
+            if(Character.isUpperCase(str.charAt(i)))
+                stack.push(str.charAt(i));
+            else {
+                if (stack.isEmpty())
+                    break;
+                char topStack = stack.peek();
+                topStack = Character.toLowerCase(topStack);
+                if (topStack == str.charAt(i)) {
+                    //index = i;
+                    stack.pop();
+
+                } else
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "ABCcab";
+        boolean output = stringMatching(str);
+        if(output)
+            System.out.println("The String has Matching Pairs");
+        else
+            System.out.println("The String has no Matching Pairs");
+    }
+}
