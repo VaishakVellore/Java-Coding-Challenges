99a8b71a96d8f8de1acc692e61702f5015acbdae
VaishakVellore <vaishak.vellore@gmail.com>
2019-03-02 14:19:28 -0700

    Adding FindSubstring problem

---
+++ src/FacebookInterviewkit/FindSubstring.java
@@ -0,0 +1,32 @@
+package FacebookInterviewkit;
+
+import java.util.HashMap;
+
+public class FindSubstring {
+
+    static HashMap<String, Integer> set = new HashMap<>();
+
+    static void findallSubstrings(String s)
+    {
+        for(int i = 0; i<s.length(); i++)
+        {
+            for(int j = i+1; j<=s.length(); j++)
+                set.put(s.substring(i,j),i);
+        }
+    }
+
+    static void findString(String b){
+        if(set.get(b) != null)
+            System.out.println(set.get(b));
+        else
+            System.out.println(-1);
+    }
+
+    public static void main(String[] args) {
+        String a = "GeeksForGeeks";
+        String b = "Fr";
+
+        findallSubstrings(a);
+        findString(b);
+    }
+}
